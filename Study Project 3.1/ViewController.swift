41c8ddc2ca0b24e13708690ca47820079b2efd66
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-06-26 12:51:41 +0300

    study project about animation and spring framework

--- Study Project 3.1/ViewController.swift
+++ Study Project 3.1/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SpringAnimation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: UIViewController {
+    
+    @IBOutlet var coreAnimationView: UIView!
+    
+    private var animationStarted = false
+    
+    @IBOutlet var springAnimationView: SpringView!
+    
+    @IBAction func startCoreAnimation(_ sender: UIButton) {
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: [.autoreverse, .repeat]) { [unowned self] in
+                if !animationStarted {
+                    coreAnimationView.frame.origin.x -= 40
+                    animationStarted.toggle()
+                }
+            }
+        
+        sender.pulsate()
     }
-
-
+    @IBAction func startSpringAnimation(_ sender: SpringButton) {
+        springAnimationView.animation = "shake"
+        springAnimationView.curve = "easeOut"
+        springAnimationView.force = 2
+        springAnimationView.animate()
+        
+        sender.animation = "squeeze"
+        sender.animate()
+    }
 }
 
