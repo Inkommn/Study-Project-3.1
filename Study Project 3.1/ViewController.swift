3f038f4d2a447001932fb0f39e670be56a6a3852
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-06-26 10:25:34 +0300

    Initial Commit

---
+++ Study Project 3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study Project 3.1
+//
+//  Created by Shamkhan Mutuskhanov on 26.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
